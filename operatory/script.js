830adbe3c12c0684017bac0ca2da5a7973607386
JetBlack-UI-UI <bekob9588@gmail.com>
2026-01-09 18:20:04 +0500

    Add files via upload

---
+++ operatory/script.js
@@ -0,0 +1,54 @@
+let a = 10
+let b = 5
+let c = 2
+
+let sum = a + b + c
+console.log(sum)
+let sum1 = a + c - b
+console.log(sum1)
+let sum2 = a * b / c
+console.log(sum2)
+let sum3 = a % b 
+console.log(sum3)  
+
+
+let x = 7
+console.log(x++ + ++x)
+
+x = 7
+console.log(x-- - --x)
+
+
+
+
+let sum4 = 4 + 5; 
+console.log(sum); 
+
+let text = "The result is: " + sum4;
+console.log(text);
+
+
+
+
+let d = 10;
+let g = '10';
+console.log(a == b);
+//== — сравнивает значения, тип данных не важен.
+//=== — сравнивает и значения, и тип данных.
+console.log(a === b); 
+
+
+
+let x1 = 15;
+
+console.log((x1%2 === 0 || x1%3 === 0) && x1 %6 !=0);
+
+
+
+
+let y1 = 40
+
+console.log(!(y1 > 10 || y1 < 5) );
+
+
+
