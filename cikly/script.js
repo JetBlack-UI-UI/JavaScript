bfd29ea147b264c40cccf4ab20643d269cc719b6
JetBlack-UI-UI <bekob9588@gmail.com>
2026-01-23 17:30:38 +0500

    Add files via upload

---
+++ cikly/script.js
@@ -0,0 +1,85 @@
+// Task 1
+// console.log("Task 1");
+// let a = 1;
+// while (a <= 10) {
+//     console.log(a);
+//     a = a + 1;
+// }
+
+// Task 2
+// console.log("Task 2");
+// let b = 1;
+// while (b <= 10) {
+//     if (b == 5) {
+//         b = b + 1;
+//         continue;
+//     }
+//     console.log(b);
+//     if (b == 8) {
+//         break;
+//     }
+//     b = b + 1;
+// }
+
+// Task 3
+// console.log("Task 3");
+// let c = 1;
+// do {
+//     console.log(c);
+//     c = c + 1;
+// } while (c <= 5);
+
+// Task 4 
+// console.log("Task 4 ");
+// while (true) {
+//     let answer = prompt("(10 = stop)");
+//     if (answer == "10") {
+//         break;
+//     }
+// }
+
+// Task 5
+// console.log("Task 5");
+// for (let d = 1; d <= 10; d = d + 1) {
+//     console.log(d);
+// }
+
+// Task 6
+// console.log("Task 6");
+// let e = 0;
+// for (;;) {
+//     e = e + 1;
+//     console.log(e);
+//     if (e > 100) {
+//         break;
+//     }
+// }
+
+// Task 7
+// console.log("Task 7");
+// let numbers1 = [1, 2, 3, 4, 5];
+
+// for (let f = 0; f < 5; f = f + 1) {
+//     console.log(numbers[f]);
+// }
+
+// Task 8 
+
+//while kogda est uslovie 
+//for kogda kogda znayu skolko budet deistvii
+
+
+// Task 9
+// console.log("Task 9");
+// for (let g = 10; g >= 1; g = g - 1) {
+//     console.log(g);
+// }
+
+// Task 10
+// console.log("Task 10");
+// for (let h1 = 1; h <= 10; h = h + 1) {
+//     if (h % 2 == 1) {
+//         continue;
+//     }
+//     console.log(h);
+// }
